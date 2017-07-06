5ecfbfe6ed408f290b57a5372699e1ff3db1cbd4
Patrick Luboobi <patricklu2010@gmail.com>
2017-07-06 11:19:54 +0300

    Create init method for BinarySearch class

--- binary_search.py
+++ binary_search.py
@@ -1,2 +1,20 @@
 class BinarySearch(list):
-    pass
+    """
+    Class to search for a value using binary search
+    """
+
+    def __init__(self, a, b) -> None:
+        """
+        Constructor for the class
+        Args:
+            a: length of list
+            b: step
+        """
+        super().__init__()
+        self.a = a
+        self.b = b
+
+        # Generate list and assign it to class
+        for x in range(1, a + 1):
+            self.append(x * b)
+        self.length = len(self)
